b24e8c4182b0b1407183be3d6ffe042246fd4756
ImageVisioner <ImageVisioner@outlook.com>
2024-01-17 22:18:03 +0800

    IPI
    IPI

--- compareMethod/01-IPI/main.m
+++ compareMethod/01-IPI/main.m
@@ -1,26 +1,3 @@
-% This code is an implementation of the small target detecton algorithm in our published paper: Chenqiang Gao, Deyu Meng, Yi Yang, et al., "Infrared Patch-Image Model for Small Target Detection in a Single Image, " Image Processing, IEEE Transactions on, vol. 22, no. 12, pp. 4996-5009, 2013.
-% please note that this code do not contain the segmentation step. If you
-% want to get the locations of small targets, you need to use some
-% segmentation algorithm.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% If you use this code in your publications, please cite:
-% Chenqiang Gao, Deyu Meng, Yi Yang, et al., "Infrared Patch-Image Model for Small Target Detection in a Single Image, " Image Processing, IEEE Transactions on, vol. 22, no. 12, pp. 4996-5009, 2013. 
-% @article{Gao2013,
-%    author = {Gao, Chenqiang and Meng, Deyu and Yang, Yi and Wang, Yongtao and Zhou, Xiaofang and Hauptmann, Alex},
-%    title = {Infrared Patch-Image Model for Small Target Detection in a Single Image},
-%    journal = {Image Processing, IEEE Transactions on},
-%    volume = {22},
-%    number = {12},
-%    pages = {4996-5009},
-%    year = {2013}
-% }
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% If you have any questions, please contact:
-% Author: Chenqiang Gao 
-% Email: gaochenqiang@gmail.com or gaocq@cqupt.edu.cn
-% Copyright:  Chongqing University of Posts and Telecommunications
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% License: Our code is only available for non-commercial research use.
 
 clc;
 close all;
