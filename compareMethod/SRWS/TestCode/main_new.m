727c362982a78c2fe70e33c9a306cf4b6002905a
ImageVisioner <ImageVisioner@outlook.com>
2024-01-18 14:06:48 +0800

    1
    1

--- compareMethod/SRWS/TestCode/main_new.m
+++ compareMethod/SRWS/TestCode/main_new.m
@@ -1,21 +1,53 @@
-
-
-clc;    clear; 
+clc;   
+clear; 
 close all;
 
-Img = imread('images\1.bmp');
+% 定义输入输出路径
+input_folder = 'images';
+output_folder_target = 'result/target';
+output_folder_background = 'result/background';
 
-if ndims(Img) == 3
-    Img = rgb2gray(Img);
+% 如果输出目录不存在，则创建它们
+if ~exist(output_folder_target, 'dir')
+    mkdir(output_folder_target);
 end
-Img = im2double(Img);
-figure,subplot(121),imshow(Img),title('Original Image');
+if ~exist(output_folder_background, 'dir')
+    mkdir(output_folder_background);
+end
 
+% 获取图像文件列表
+image_files = dir(fullfile(input_folder, '*.bmp'));
+
+% 初始化算法对象
 alg = SRWS;
-alg = alg.process(Img);
-subplot(122),imshow(alg.result, []),title('result');
 
-figure,
-subplot(131),imshow(alg.rstB, []),title('Background');
-subplot(132),imshow(alg.rstE, []),title('Noise');
-subplot(133),imshow(alg.Z, []),title('Coefficient Matrix');
+% 遍历图像文件进行处理
+for i = 1:length(image_files)
+    img_name = image_files(i).name;
+    img_path = fullfile(input_folder, img_name);
+    
+    % 读取图像
+    Img = imread(img_path);
+
+    if ndims(Img) == 3
+        Img = rgb2gray(Img);
+    end
+    Img = im2double(Img);
+    
+    % 处理图像
+    alg = alg.process(Img);
+    
+    % 保存结果
+    imwrite(alg.result, fullfile(output_folder_target, img_name));
+    imwrite(alg.rstB, fullfile(output_folder_background, img_name));
+    % 
+    % % 显示原始图像和处理结果
+    % figure;
+    % subplot(131), imshow(Img), title('Original Image');
+    % subplot(132), imshow(alg.result, []), title('result');
+    % subplot(133), imshow(alg.rstB, []), title('Background');
+    % 
+    % %适当等待以显示图像
+    % pause(1);
+end
+disp("done!")
